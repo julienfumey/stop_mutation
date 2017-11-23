84fa67d49ca2e5d42c3660d1291c427d34908dda
marsu15 <julien.fumey@gmail.com>
2017-11-23 14:58:00 +0100

    change migration handling

--- stop_mutation_simulator.py
+++ stop_mutation_simulator.py
@@ -15,14 +15,27 @@ def initData(seqLength):
         listePos.append({"seqSize":i,"freq":0.0,"nb":0}) #size,freq muta,nb muta
     return(listePos)
 
+def migrate(freq, param):
+    if param["txmigra"] != 0:
+        print(freq)
+        print("Migra L")
+        freq *= (1-(npr.binomial(2*param["popSize"],param["txmigra"])/(2*param["popSize"])))
+        print(freq)
+    elif random.random() < param["probMigra"]:
+        print("Migra F")
+        freq *= (1-param["indMigra"])
+
+    return(freq)
+
 def evolve(pos, param):
     probMuta = param["txMut"] * pos["seqSize"] * param["freqMutaStop"] * 2*param["popSize"] * (1-pos["freq"])
     if(random.random() < probMuta):
         pos["freq"] += 1/(2*param["popSize"])
         pos["nb"] += 1
-    if(random.random() < param["probMigra"]):
-        pos["freq"] = pos["freq"]*(1-param["indMigra"])
-    
+
+    if(pos["freq"] > 0):
+        pos["freq"] = migrate(pos["freq"], param)
+
     pos["freq"] = npr.binomial(2*param["popSize"],pos["freq"]) / (2*param["popSize"])
     return(pos)
 
@@ -35,10 +48,21 @@ parser.add_argument('-r', '--repeats', type=int, required=True, help="Number of 
 parser.add_argument('-m', '--mu', type=float, required=True, help="Mutation rate")
 parser.add_argument('-N', '--Ne', type=int, required=True, help="Effective population size")
 parser.add_argument('-s', '--stop', type=float, required=True, help="Frequency of stop mutations")
-parser.add_argument('-f', '--freqmigra', type=float, required=True, help="Frequency of migration event")
-parser.add_argument('-F', '--migrant', type=float, required=True, help="Frequency of migrant fish")
+parser.add_argument('-f', '--freqmigra', type=float, required=False, help="Frequency of migration event", default=0)
+parser.add_argument('-F', '--migrant', type=float, required=False, help="Frequency of migrant fish", default=0)
+parser.add_argument('-l', '--tauxmigra', type=float, required=False,help="Combination of frequency of migration event and of migrant fish", default=0)
 
 args = parser.parse_args()
+
+if args.tauxmigra == 0 and ((args.freqmigra == 0 and args.migrant != 0) or (args.freqmigra != 0 and args.migrant == 0)):
+    args.freqmigra = 0
+    args.migrant = 0
+    print("Assuming no migration")
+
+if args.tauxmigra != 0 and (args.freqmigra != 0 or args.migrant != 0):
+    print("ERROR : option -l can't be used together with -F and -f. Quitting")
+    sys.exit()
+
 
 param = {"nbGeneration":args.generations,
 "nbRepeats":args.repeats,
@@ -46,7 +70,9 @@ param = {"nbGeneration":args.generations,
 "freqMutaStop":args.stop,
 "popSize":args.Ne,
 "probMigra":args.freqmigra,
-"indMigra":args.migrant}
+"indMigra":args.migrant,
+"txmigra":args.tauxmigra
+}
 
 
 seqLength = (1050,1011,1056,1158,1059,1218,1056,966,861,945,1101,1221,1071,2802,1653,1227,2520,1062,1011,1146,1164,1227,2958,1197,1047,1881,891,882,1743)

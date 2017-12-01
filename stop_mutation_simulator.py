97450bc9c2c55ab08823876a7cbfed87c78b0859
marsu15 <julien.fumey@gmail.com>
2017-12-01 18:00:25 +0100

    add selection

--- stop_mutation_simulator.py
+++ stop_mutation_simulator.py
@@ -17,24 +17,35 @@ def initData(seqLength):
         listePos.append({"seqSize":i,"freq":0.0,"nb":0}) #size,freq muta,nb muta
     return(listePos)
 
-def migrate(listePos, param):
+"""def migrate(listePos, param):
     if param["txmigra"] != 0:
         for pos in listePos:
             if pos["freq"] > 0:
-                pos["freq"] *= (1-(npr.binomial(2*param["popSize"],param["txmigra"])/(2*param["popSize"])))
+                pos["freq"] *= (1-())
     elif random.random() < param["probMigra"]:
         for pos in listePos:
             if pos["freq"] > 0:
                 pos["freq"] *= (1-param["indMigra"])
-    return(listePos)
+    return(listePos)"""
 
-def evolve(pos, param):
+def drift(freq,popSize):
+    return npr.binomial(2*popSize,freq)/(2*popSize)
+
+def selection(freq,param,freqMigra):
+    averageSelection = ((1-freq)**2)*(1-freqMigra) + freqMigra + 2*freq*(1-freq)*(1-param["h"]*param["fitness"])*(1-freqMigra) + (freq**2)*(1-param["fitness"])*(1-freqMigra)
+    newFreq = (((1-freq)**2)*(1-freqMigra)+freqMigra + freq*(1-freq)*(1-param["h"]*param["fitness"])*(1-freqMigra))/averageSelection
+    return(newFreq)
+
+def evolve(pos, param, freqMigra):
+    pos["freq"] = selection(pos["freq"], param, freqMigra)
+
     probMuta = param["txMut"] * pos["seqSize"] * param["freqMutaStop"] * 2*param["popSize"] * (1-pos["freq"])
     if(random.random() < probMuta):
         pos["freq"] += 1/(2*param["popSize"])
         pos["nb"] += 1
 
-    pos["freq"] = npr.binomial(2*param["popSize"],pos["freq"]) / (2*param["popSize"])
+
+    pos["freq"] = drift(pos["freq"],param["popSize"])
     return(pos)
 
 def mutationCounter(listeFreq):
@@ -49,6 +60,8 @@ parser.add_argument('-s', '--stop', type=float, required=True, help="Frequency o
 parser.add_argument('-f', '--freqmigra', type=float, required=False, help="Frequency of migration event", default=0)
 parser.add_argument('-F', '--migrant', type=float, required=False, help="Frequency of migrant fish", default=0)
 parser.add_argument('-l', '--tauxmigra', type=float, required=False,help="Combination of frequency of migration event and of migrant fish", default=0)
+parser.add_argument('-S', '--fitness', type=float, required=False, default=0, help="Fitness of mutated alleles")
+parser.add_argument('-H', '--dominance', type=float, required=False, default=0, help="Dominance of mutated alleles")
 
 args = parser.parse_args()
 
@@ -69,7 +82,9 @@ param = {"nbGeneration":args.generations,
 "popSize":args.Ne,
 "probMigra":args.freqmigra,
 "indMigra":args.migrant,
-"txmigra":args.tauxmigra
+"txmigra":args.tauxmigra,
+"fitness":args.fitness,
+"h":args.dominance
 }
 
 
@@ -88,13 +103,18 @@ for j in range(param["nbRepeats"]):
     sys.stderr.flush()
     listePos = initData(seqLength)
     for i in range(param["nbGeneration"]):
+        freqMigra = 0
+        if random.random() < param["probMigra"]:
+            freqMigra = param["indMigra"]
+        elif param["txmigra"] != 0:
+            freqMigra = npr.binomial(2*param["popSize"],param["txmigra"])/(2*param["popSize"])
+
         for pos in listePos:
-            pos = evolve(pos, param)
-        listePos = migrate(listePos, param)
+            pos = evolve(pos, param, freqMigra)
+
         if(i % 10 == 0):
             #print(i)
             b = [el["freq"] for el in listePos]
-            #print(mutationCounter(b))
             countMutaMatrix[(i//10),mutationCounter(b)] += 1
 
 
